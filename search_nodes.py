3e0b47ae21332936f94cadb89b90922485d012d9
Joran Martijn <joranmartyn@gmail.com>
2020-01-06 18:57:29 -0400

    more traversing and searching

--- search_nodes.py
+++ search_nodes.py
@@ -28,3 +28,115 @@ print(len(nodes), "nodes have distance=0.5")
 # also .get_leaves_by_name returns a list!
 D = t.get_leaves_by_name(name="D")[0]
 print(D)
+
+## you can also create your own search function
+# for when .search_nodes method is not complex search conditions enough
+def search_by_size(node,size):
+    "Finds nodes with a given number of leaves"
+    
+    # create an empty list
+    matches = []
+
+    # fill list with nodes that fullfill the criteria of having <size> number of leaves
+    for n in node.traverse():
+        if len(n) == size:
+            print("Found one!")
+            matches.append(n)
+    return matches
+
+# apply function on random tree
+t = Tree()
+# names_library only names leaves?
+t.populate(40, names_library=["A","B","C","D"], reuse_names=True)
+print(t)
+print( t.write(format=1) )
+# search_by_size() thus returns a list of TreeNode objects that fullfill the set criteria
+matches = search_by_size(t, size=4)
+# view the nodes by printing their corresponding subtrees
+for node in matches:
+    print(node)
+
+## Find the last common ancestor of a set of nodes
+t = Tree( "((H:0.3,I:0.1):0.5, A:1, (B:0.4,(C:0.5,(J:1.3, (F:1.2, D:0.1):0.5):0.5):0.5):0.5);" )
+print(t)
+#       /-H
+#    /-|
+#   |   \-I
+#   |
+# --|--A
+#   |
+#   |   /-B
+#    \-|
+#      |   /-C
+#       \-|
+#         |   /-J
+#          \-|
+#            |   /-F
+#             \-|
+#                \-D
+lca = t.get_common_ancestor("C","J","B")
+# remember that lca is a TreeNode object, so printing it returns the entire subtree
+print(lca)
+
+## another way to search for particular nodes is to combine the python filter() function with the .traverse method:
+t = Tree("((H:0.3,I:0.1):0.5, A:1, (B:0.4,(C:1,D:1):0.5):0.5);")
+
+# print branch lengths
+for node in t.traverse():
+    print(node.name, node.dist)
+print("====\n")
+#  0.0
+#  0.5
+# A 1.0
+#  0.5
+# H 0.3
+# I 0.1
+# B 0.4
+#  0.5
+# C 1.0
+# D 1.0
+# ====
+
+def is_long_branch(node):
+    if node.dist > 0.6:
+        return True
+    else:
+        return False
+
+# filter() applies a True/False function to all elements in a specified iterable object,
+# and returns only the True items in a 'filter object', which is an iterator
+matches = filter(is_long_branch, t.traverse())
+
+# to find the length you need to iterate over the filter object
+# you can do this apparently with the list() function
+print(len(list(matches)), "nodes have branches with length > 0.3")
+
+## Finding TreeNode objects by their name is so common that ETE3 implements a shortcut: TreeNODE&NodeName
+t = Tree("((H:0.3,I:0.1):0.5, A:1, (B:0.4,(C:1,(J:1, (F:1, D:1):0.5):0.5):0.5):0.5);")
+# returns the first TreeNode whose name is "D"
+print(t)
+#       /-H
+#    /-|
+#   |   \-I
+#   |
+# --|--A
+#   |
+#   |   /-B
+#    \-|
+#      |   /-C
+#       \-|
+#         |   /-J
+#          \-|
+#            |   /-F
+#             \-|
+#                \-D
+
+D = t&"D"
+print(D)
+
+node = D
+path = []
+while node.up:
+    path.append(node)
+    node = node.up
+print(path)
--- tree_traversing.py
+++ tree_traversing.py
@@ -4,16 +4,72 @@ from ete3 import Tree
 # load a tree
 t = Tree('((((H,K)D,(F,I)G)B,E)A,((L,(N,Q)O)J,(P,S)M)C);', format=1)
 print(t)
+#             /-H
+#          /-|
+#         |   \-K
+#       /-|
+#      |  |   /-F
+#    /-|   \-|
+#   |  |      \-I
+#   |  |
+#   |   \-E
+# --|
+#   |      /-L
+#   |   /-|
+#   |  |  |   /-N
+#   |  |   \-|
+#    \-|      \-Q
+#      |
+#      |   /-P
+#       \-|
+#          \-S
 
-# traverse the tree nodes in postorder
+# .traverse returns an 'iterator' (a list? an iterable object?) of the tree nodes in postorder
 # postorder: traverse left subtree from leave to top, traverse right subtree from leave to top, visit the root
 for node in t.traverse("postorder"):
     print(node.name)
+# H
+# K
+# D
+# F
+# I
+# G
+# B
+# E
+# A
+# L
+# N
+# Q
+# O
+# J
+# P
+# S
+# M
+# C
+# 
 
 # traverse over the tree in postorder, but skip the root node
 # the difference in output is one less empty line (this line represents the root node that did not have a name)
 for node in t.iter_descendants("postorder"):
     print(node.name)
+# H
+# K
+# D
+# F
+# I
+# G
+# B
+# E
+# A
+# L
+# N
+# Q
+# O
+# J
+# P
+# S
+# M
+# C
 
 t = Tree( "(A:1,(B:1,(C:1,D:1):0.5):0.5);" )
 print(t)
