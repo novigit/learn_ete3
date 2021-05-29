db7db55a94146e70d189321e5aba0e1028d84252
Joran Martijn <joranmartyn@gmail.com>
2021-05-29 15:39:43 -0300

    scratch.py - fix clade coloring

--- scratch.py
+++ scratch.py
@@ -10,6 +10,7 @@ t = Tree("rp56.nw")
 ts = TreeStyle()
 ts.show_leaf_name = False
 ts.show_branch_support = True
+ts.scale_length = 0.3
 ts.title.add_face(TextFace("Tree Title", fsize=20), column=0)
 
 
@@ -27,12 +28,12 @@ for node in t.traverse():
 ## taxon - clade - color
 with open("scratch.map", "r") as m:
     map = {}
-    for taxon, clade, color in csv.reader(m, delimiter='\t'):
+    for taxon, clade, color in csv.reader(m, delimiter="\t"):
         map[taxon] = [clade, color]
 
 
 # Root tree
-outgroup_taxa = [ taxon for taxon in map.keys() if map[taxon][0] == 'Outgroup' ]
+outgroup_taxa = [taxon for taxon in map.keys() if map[taxon][0] == "Outgroup"]
 outgroup_lca = t.get_common_ancestor(outgroup_taxa)
 t.set_outgroup(outgroup_lca)
 
@@ -48,16 +49,22 @@ def leaf_font(node):
     and replace underscores with spaces
     """
     if node.is_leaf():
-        if node.name in outgroup_taxa:
-            node.name = node.name.replace("_", " ")
-            leaf_face = AttrFace(attr="name", ftype="Arial", fgcolor="grey")
-        else:
-            node.name = node.name.replace("_", " ")
-            leaf_face = AttrFace(attr="name", ftype="Arial", fgcolor="black")
 
+        # extract color from loaded mapping file
+        color = map[node.name][1]
+
+        # remove underscores
+        node.name = node.name.replace("_", " ")
+
+        # color leaf names
+        leaf_face = AttrFace(attr="name", ftype="Helvetica", fgcolor=color)
         faces.add_face_to_node(
-            face=leaf_face, node=node, column=0, position="branch-right"
+            face=leaf_face, node=node, column=1, position="branch-right"
         )
 
+    # else:
+    #     support_face = AttrFace(attr="support", ftype="Arial", fsize=8, fgcolor="crimson")
+    #     faces.add_face_to_node(face=support_face, node=node, column=0, position="branch-top")
 
-t.show(tree_style=ts, layout=leaf_font)
+# t.render("scratch.pdf", tree_style=ts, layout=leaf_font, w=210, units="mm")
+t.render("scratch.pdf", tree_style=ts, layout=leaf_font)
